e2e24b41d0f8a0ae2d888af8c8424c492eac32b3
pavel konoplyanikov <p.konoplyanikov@naveksoft.com>
2025-06-03 13:13:58 +0300

    get any keys

--- src/GetPrivateKeys.php
+++ src/GetPrivateKeys.php
@@ -6,15 +6,22 @@ use Predis\Client;
 
 class GetPrivateKeys
 {
-    public function getKeys(array $config): array
+    public function getPublicKeys(array $config): array
     {
         $client = new Client($config);
-        return $client->hgetall('keys');
+        return $client->hgetall('public_keys');
     }
 
-    public function setKeys(array $config, string $key, string $value): void
+    public function getPrivateKeys(array $config): array
     {
         $client = new Client($config);
-        $client->hset('keys', $key, $value);
+        return $client->hgetall('private_keys');
+    }
+
+    public function setKeys(array $config, string $key, string $publicKey, string $privateKey): void
+    {
+        $client = new Client($config);
+        $client->hset('public_keys', $key, $publicKey);
+        $client->hset('private_keys', $key, $privateKey);
     }
 }
